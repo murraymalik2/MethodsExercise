d4ab297acde1833eab0b186e00b716786f5dca3f
murra <murraymalik2@gmail.com>
2023-06-14 18:28:44 -0400

    Methods exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Greetings -What is your first name?"); // output
+
+            var userName = Console.ReadLine(); // input
+
+            Console.WriteLine($"Hi {userName}. What is your favorite color?"); // output
+
+            var color = Console.ReadLine(); // input
+
+            Console.WriteLine($"{color} is a great color! What's your favorite animal? "); // output
+
+            var animal = Console.ReadLine(); // input
+
+            Console.WriteLine($"Okay you like the {animal} the best? That's cool! What's your favorite band?"); // output
+
+            var band = Console.ReadLine(); // input
+
+            Console.WriteLine($"{band} is a pretty neat band!");
+
         }
     }
 }
